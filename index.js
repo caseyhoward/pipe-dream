7f8eca9be651081c737aa927e8f1160b94d6733f
Casey Howard <caseyhoward@caseyhoward.com>
2015-02-28 14:23:11 -0800

    Blah

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ var NpmModuleInstaller = require('./src/npm_module_installer');
 var npmModuleInstaller = new NpmModuleInstaller();
 var gulpPluginRegistry = new GulpPluginRegistry(npmModuleInstaller, require);
 
-module.exports = (new PipeDreamCreator(gulpPluginRegistry, npmModuleInstaller)).create;
+module.exports = (new PipeDreamCreator(gulpPluginRegistry)).create;
--- src/pipe_dream_creator.js
+++ src/pipe_dream_creator.js
@@ -1,13 +1,27 @@
 var Proxy = require('harmony-proxy');
 var GulpPluginRunner = require('./gulp_plugin_runner');
 var Promise = require('es6-promise').Promise;
+var gulp = require('gulp');
 
 module.exports = PipeDreamCreator;
 
-function PipeDreamCreator(gulpPluginRegistry) {
+function PipeDreamCreator(gulpPluginRegistry, streamProxyCreator) {
+  streamProxyCreator = streamProxyCreator || new StreamProxyCreator();
   this.create = function(globString, options) {
+    var stream = gulp.src(globString);
+    return streamProxyCreator.create(stream, {}, function(functionName) {
+      // TODO: Find out exactly why this hack is needed.
+      // It looks like gulp maybe calls 'then' on the return value of tasks to see if they are promises.
+      if (functionName !== 'then') {
+        return gulpPluginRegistry.get(functionName);
+      }
+    });
+  };
+};
+
+function StreamProxyCreator() {
+  this.create = function(stream, options, callback) {
     var object = {};
-    var gulpPluginRunner = GulpPluginRunner.create(globString, options, gulpPluginRegistry);
     var lastCommand = new Promise(function(resolve) { resolve(); });
 
     var proxy = new Proxy(object, {
@@ -15,12 +29,10 @@ function PipeDreamCreator(gulpPluginRegistry) {
         return function() {
           var pluginArguments = Array.prototype.slice.call(arguments);
 
-          // TODO: Find out exactly why this hack is needed.
-          // It looks like gulp maybe calls 'then' on the return value of tasks to see if they are promises.
-          if (name === 'then') return;
-
           lastCommand = lastCommand.then(function() {
-            return gulpPluginRunner.run(name, pluginArguments);
+            return callback(name).then(function(plugin) {
+              stream = stream.pipe(plugin.apply(null, pluginArguments));
+            });
           });
 
           return proxy;
@@ -30,5 +42,4 @@ function PipeDreamCreator(gulpPluginRegistry) {
 
     return proxy;
   };
-};
-
+}
