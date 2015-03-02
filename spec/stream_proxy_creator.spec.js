1c7201d6353d6ab20a514714835b49b065eff551
Casey Howard <caseyhoward@caseyhoward.com>
2015-03-01 20:29:56 -0800

    Add support for promises to StreamProxyCreator

--- spec/stream_proxy_creator.spec.js
+++ spec/stream_proxy_creator.spec.js
@@ -1,9 +1,10 @@
 var sinon = require('sinon');
 var expect = require('chai').expect;
+var Promise = require('es6-promise').Promise;
 
 var StreamProxyCreator = require('../src/stream_proxy_creator');
 
-describe.only('StreamProxyCreator', function() {
+describe('StreamProxyCreator', function() {
   var streamProxy, callback;
 
   beforeEach(function() {
@@ -11,29 +12,74 @@ describe.only('StreamProxyCreator', function() {
     streamProxy = (new StreamProxyCreator()).create(callback);
   });
 
-  it('calls the callback when called directly', function() {
+  it('calls the callback when called directly', function(done) {
     streamProxy();
-    expect(callback.getCall(0).args).to.deep.equal([[], []]);
+    setTimeout(function() {
+      expect(callback.getCall(0).args).to.deep.equal([[], []]);
+      done();
+    }, 0);
   });
 
-  it('calls the callback with arguments when called directly', function() {
+  it('calls the callback with arguments when called directly', function(done) {
     streamProxy('some', 'args');
-    expect(callback.getCall(0).args).to.deep.equal([[], ['some', 'args']]);
+    setTimeout(function() {
+      expect(callback.getCall(0).args).to.deep.equal([[], ['some', 'args']]);
+      done();
+    }, 0);
   });
 
-  it('calls the callback with the function name', function() {
+  it('calls the callback with the function name', function(done) {
     streamProxy.a();
-    expect(callback.getCall(0).args).to.deep.equal([['a'], []]);
+    setTimeout(function() {
+      expect(callback.getCall(0).args).to.deep.equal([['a'], []]);
+      done();
+    }, 0);
   });
 
-  it('calls the callback with the function name and arguments', function() {
+  it('calls the callback with the function name and arguments', function(done) {
     streamProxy.a('some', 'args');
-    expect(callback.getCall(0).args).to.deep.equal([['a'], ['some', 'args']]);
+    setTimeout(function() {
+      expect(callback.getCall(0).args).to.deep.equal([['a'], ['some', 'args']]);
+      done();
+    }, 0);
   });
 
-  it('allows chaining', function() {
+  it('allows chaining', function(done) {
     streamProxy.a.b.c('abc', 123).d.e(12);
-    expect(callback.getCall(0).args).to.deep.equal([['a', 'b', 'c'], ['abc', 123]]);
-    expect(callback.getCall(1).args).to.deep.equal([['d', 'e'], [12]]);
+    setTimeout(function() {
+      expect(callback.getCall(0).args).to.deep.equal([['a', 'b', 'c'], ['abc', 123]]);
+      expect(callback.getCall(1).args).to.deep.equal([['d', 'e'], [12]]);
+      done();
+    }, 0);
+  });
+
+  it('works with promises', function(done) {
+    var promiseAResolve;
+
+    var promiseA = new Promise(function(resolve, reject) {
+      promiseAResolve = resolve;
+    });
+
+    var callback = sinon.spy(function(callChain, callArguments) {
+      if (callChain.length === 1 && callChain[0] === 'a') {
+        return promiseA;
+      }
+    });
+
+    streamProxy = (new StreamProxyCreator()).create(callback);
+
+    streamProxy.a(1).b(2);
+
+    setTimeout(function() {
+      expect(callback.getCall(0).args).to.deep.equal([['a'], [1]]);
+      expect(callback.calledOnce).to.equal(true);
+
+      promiseAResolve();
+
+      setTimeout(function() {
+        expect(callback.getCall(1).args).to.deep.equal([['b'], [2]]);
+        done();
+      }, 0);
+    }, 0);
   });
 });
--- src/stream_proxy_creator.js
+++ src/stream_proxy_creator.js
@@ -1,12 +1,17 @@
 var Proxy = require('harmony-proxy');
+var Promise = require('es6-promise').Promise;
 
 module.exports = function StreamProxyCreator() {
   this.create = function(callback) {
     var callChain = [];
+    var promise = new Promise(function(resolve) { resolve(); });
 
     var proxy = new Proxy(function() {}, {
       apply: function(target, name, argumentList) {
-        callback(callChain, argumentList);
+        var callChainCopy = callChain.slice();
+        promise = promise.then(function() {
+          return callback(callChainCopy, argumentList);
+        });
         callChain = [];
         return proxy;
       },
