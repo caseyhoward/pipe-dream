639dad09ae3e0c2acd434225c0c221efbe189647
Casey Howard <caseyhoward@caseyhoward.com>
2015-03-03 20:35:18 -0800

    Make gulp watch not crash when tests fail

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
+var gulpUtil = require('gulp-util');
 var mocha = require('gulp-mocha');
 
 gulp.task('default', function() {
-  gulp.src('spec/**/*.spec.js', {read: false}).pipe(mocha());
+  gulp.src('spec/**/*.spec.js', {read: false}).pipe(mocha()).on('error', gulpUtil.log);
 });
 
 gulp.task('watch', ['default'], function() {
