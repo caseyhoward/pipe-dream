b3328578bb110ecc4eb3dd354508ba0f2c657a05
Casey Howard <caseyhoward@caseyhoward.com>
2015-03-03 21:38:39 -0800

    Revert "Add a prompt for installing"
    This breaks the acceptance test (obviously) and being prompted is pretty
    annoying. Maybe implement later.
    
    This reverts commit 855f3f60a54e48fd93633f8110c5c9904fad5788.

--- src/gulp_plugin_registry.js
+++ src/gulp_plugin_registry.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var Promise = require('es6-promise').Promise;
-var prompt = require('prompt');
 
 module.exports = function GulpPluginRegistry(npmModuleInstaller, requireFunction) {
   var plugins = {};
@@ -18,24 +17,10 @@ module.exports = function GulpPluginRegistry(npmModuleInstaller, requireFunction
             plugins[name] = requireFunction(gulpPluginName);
             resolve(plugins[name]);
           } catch (error) {
-            prompt.start();
-
-            prompt.get({
-              properties: {
-                install: {
-                  description: (gulpPluginName + ' not installed. Install now (Y/n)').green
-                }
-              }
-            }, function (err, result) {
-              if (result.install === 'y' || result.install === 'Y' || result.install === '') {
-                npmModuleInstaller.install(gulpPluginName).then(function(plugin) {
-                  plugins[name] = plugin;
-                  resolve(plugins[name]);
-                }, reject)
-              } else {
-                throw error;
-              }
-            });
+            npmModuleInstaller.install(gulpPluginName).then(function(plugin) {
+              plugins[name] = plugin;
+              resolve(plugins[name]);
+            }, reject)
           }
         }
       });
